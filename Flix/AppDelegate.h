a59cc3527274ce30043263385d912135eb4b71d7
Trustin Harris <trust4eva@fb.com>
2018-06-27 11:06:50 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Trustin Harris on 6/27/18.
+//  Copyright © 2018 Trustin Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
